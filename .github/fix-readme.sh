39c72507be035e9cf151a89975afc4064d3514a4
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-06-20 10:06:21 +0200

    fix-readme

--- .github/fix-readme.sh
+++ .github/fix-readme.sh
@@ -5,6 +5,7 @@ repos=("Aliucord-RightNow-Plugins" "aliucord-plugins" "aliucord-sendembeds" "Goo
 echo "# [C10udburst](https://github.com/C10udburst)'s Discord related code" > profile/README.md
 
 for repo in "${repos[@]}"; do
-    curl "https://raw.githubusercontent.com/c10udburst-discord/${repo}/master/README.md" -s | sed -r "s/^# (.+)/## [\1]\(https:\/\/github.com\/c10udburst-discord\/${repo}\)/" >> profile/README.md
+    replacement="## [\1]\(https:\/\/github.com\/c10udburst-discord\/${repo}\)\n![GitHub Repo stars](https:\/\/img.shields.io\/github\/stars\/c10udburst-discord\/${repo}?color=gold\&style=plastic)  ![GitHub Repo forks](https:\/\/img.shields.io\/github\/forks\/c10udburst-discord\/${repo}?color=%23986ee2\&style=plastic)"
+    curl "https://raw.githubusercontent.com/c10udburst-discord/${repo}/master/README.md" -s | sed -r "s/^# (.+)/${replacement}/" >> profile/README.md
     echo "" >> profile/README.md
 done
