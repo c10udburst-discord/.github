9dd733c79d850e8ec5e79a0f6ceec06188dbda2e
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-07-10 08:35:11 +0200

    Update fix-readme.sh

--- .github/fix-readme.sh
+++ .github/fix-readme.sh
@@ -5,7 +5,7 @@ repos=("Aliucord-RightNow-Plugins" "aliucord-plugins" "aliucord-sendembeds" "Goo
 echo "# [C10udburst](https://github.com/C10udburst)'s Discord related code" > profile/README.md
 
 for repo in "${repos[@]}"; do
-    replacement="## [\1]\(https:\/\/github.com\/c10udburst-discord\/${repo}\)\n![GitHub Repo stars](https:\/\/img.shields.io\/github\/stars\/c10udburst-discord\/${repo}?color=gold\&style=plastic)  ![GitHub Repo forks](https:\/\/img.shields.io\/github\/forks\/c10udburst-discord\/${repo}?color=%23986ee2\&style=plastic)  ![GitHub last commit](https:\/\/img.shields.io\/github\/last-commit\/c10udburst-discord\/${repo}?style=plastic)"
+    replacement="## [\1]\(https:\/\/github.com\/c10udburst-discord\/${repo}\)\n![GitHub Repo stars](https:\/\/img.shields.io\/github\/stars\/c10udburst-discord\/${repo}?color=gold\&style=plastic)  ![GitHub Repo forks](https:\/\/img.shields.io\/github\/forks\/c10udburst-discord\/${repo}?color=%23986ee2\&style=plastic)  ![GitHub last commit](https:\/\/img.shields.io\/github\/last-commit\/c10udburst-discord\/${repo}?style=plastic)\n<details>\n"
     curl "https://raw.githubusercontent.com/c10udburst-discord/${repo}/master/README.md" -s | sed -r "s/^# (.+)/${replacement}/" >> profile/README.md
-    echo "" >> profile/README.md
+    echo -e "\n</details>\n" >> profile/README.md
 done
